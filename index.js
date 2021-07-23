1f4d680b6d6e643b44f22eee8cff67aadcb2c5a4
Vitaliy Kybych <vitaliykyq@gmail.com>
2021-07-23 14:00:33 +0300

    Edit HTML File, and test deploy

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const fs = require("fs");
+
 let express = require("express");
 let app = express();
 
 
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
+    response.send(fs.readFileSync("index.html", "utf8"));
 });
 
 let port = process.env.PORT || 5000;
