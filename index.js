2bcfc29ece33b133415a9219d4a31abe8b6a5360
Vitaliy Kybych <vitaliykyq@gmail.com>
2021-07-23 13:01:00 +0300

    Start site with nodeJS

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk');
-const myTeleBot = require('./telegramBot/telegramBot');
+const http = require("http");
 
-let bot = new myTeleBot.TelegramBot("TEST SITE");
+const host = 'localhost';
+const port = 8880;
 
-bot.startBot().then();
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(`<html><body><h1>Test Pages In NodeJS</h1></body></html>`);
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
